1f431f77cc90aeb64f61851ae5158928019e208d
modargo <modargo@gmail.com>
2024-12-03 13:55:53 -0500

    Add config menu with the option to turn off the cafe entry cost

--- src/main/java/spireCafe/patches/CafeEntryExitPatch.java
+++ src/main/java/spireCafe/patches/CafeEntryExitPatch.java
@@ -17,6 +17,7 @@ import javassist.NotFoundException;
 import javassist.expr.ExprEditor;
 import javassist.expr.Instanceof;
 import javassist.expr.MethodCall;
+import spireCafe.Anniv7Mod;
 import spireCafe.interactables.TestEvent;
 import spireCafe.util.ActUtil;
 
@@ -148,7 +149,7 @@ public class CafeEntryExitPatch {
         // (see DisableNormalEndOfActHealingPatch) and replicate the logic, with a multiplier to apply the cafe entry cost
         // If the net change is positive, we apply it in a single heal. If the net change is negative, we directly modify
         // the player's current health (bypassing anything that might modify healing, damage, or HP loss).
-        float entryCostMultiplier = (100 - HP_COST_PERCENT) / 100.0f;
+        float entryCostMultiplier = Anniv7Mod.getCafeEntryCostConfig() ? (100 - HP_COST_PERCENT) / 100.0f : 1.0f;
         float normalHealAmount = (AbstractDungeon.ascensionLevel >= 5 ? 0.75f : 1.0f) * (AbstractDungeon.player.maxHealth - AbstractDungeon.player.currentHealth);
         int healthChange = (int)((entryCostMultiplier * (AbstractDungeon.player.currentHealth + normalHealAmount)) - AbstractDungeon.player.currentHealth);
         if (healthChange > 0) {
