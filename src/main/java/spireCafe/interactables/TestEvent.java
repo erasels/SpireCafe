8e55f6843ca44a72d143d8f248127cb66223da1e
modargo <modargo@gmail.com>
2024-12-04 08:42:46 -0500

    Expand on the comment about future work

--- src/main/java/spireCafe/interactables/TestEvent.java
+++ src/main/java/spireCafe/interactables/TestEvent.java
@@ -28,13 +28,17 @@ public class TestEvent extends AbstractEvent {
     public void onEnterRoom() {
         // TODO: Fill out the rest of these once we have them implemented
         // 1 Bartender, 1 Merchant, ~3 total patrons and attractions (at least one of each)
+        // To prevent running out of interactables on endless, if any of the possible lists are empty or the amount of
+        // patrons/attractions isn't enough, we should clear  Anniv7Mod.currentRunSeenInteractables and try again
+        // (But doing that needs to wait until we have enough of everything for a single run to not see duplicates)
+        com.megacrit.cardcrawl.random.Random rng = AbstractDungeon.miscRng;
         List<Class<? extends AbstractCafeInteractable>> possibleBartenders = getPossibilities(AbstractBartender.class);
         List<Class<? extends AbstractCafeInteractable>> possibleMerchants = getPossibilities(AbstractMerchant.class);
         List<Class<? extends AbstractCafeInteractable>> possiblePatrons = getPossibilities(AbstractPatron.class);
         List<Class<? extends AbstractCafeInteractable>> possibleAttractions = getPossibilities(AbstractAttraction.class);
 
         int numPatrons = 3;
-        Collections.shuffle(possiblePatrons, new java.util.Random(AbstractDungeon.miscRng.randomLong()));
+        Collections.shuffle(possiblePatrons, new java.util.Random(rng.randomLong()));
         for (int i = 0; i < numPatrons && i < possiblePatrons.size(); i++) {
             float x = (1000 + i * 200.0f) * Settings.xScale;
             float y = AbstractDungeon.floorY;
