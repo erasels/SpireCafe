71138c221a0156985d0c8e2dc7c1ed9d7591dfed
erasels <emanuelbedburdick@yahoo.de>
2024-12-16 17:20:03 +0100

    Stop speechbubble from rendering on attractions by default

--- src/main/java/spireCafe/abstracts/AbstractAttraction.java
+++ src/main/java/spireCafe/abstracts/AbstractAttraction.java
@@ -3,6 +3,7 @@ package spireCafe.abstracts;
 public abstract class AbstractAttraction extends AbstractNPC {
     public AbstractAttraction(float animationX, float animationY, float hb_w, float hb_h) {
         super(animationX, animationY, hb_w, hb_h);
+        shouldShowSpeechBubble = false;
     }
 
     //TODO: Placement Logic
