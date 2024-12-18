86bf555c7e060590a5e4c54caee28a234baaff35
erasels <emanuelbedburdick@yahoo.de>
2024-12-18 14:40:09 +0100

    Added logic to keep track of seen pages

--- src/main/java/spireCafe/interactables/attractions/bookshelf/BookshelfAttraction.java
+++ src/main/java/spireCafe/interactables/attractions/bookshelf/BookshelfAttraction.java
@@ -11,11 +11,13 @@ import spireCafe.interactables.attractions.bookshelf.pages.AbstractPage;
 import spireCafe.util.TexLoader;
 import spireCafe.util.WeightedList;
 
+import java.io.IOException;
 import java.util.*;
 
 import static spireCafe.Anniv7Mod.modID;
 
 public class BookshelfAttraction extends AbstractAttraction {
+    public static final String PAGE_CONFIG_KEY = "seenPages";
     private static final int NUM_PAGES = 3;
 
     public static final String ID = BookshelfAttraction.class.getSimpleName();
@@ -46,17 +48,56 @@ public class BookshelfAttraction extends AbstractAttraction {
     @Override
     public void onInteract() {
         List<AbstractCafeInteractable> inhabitants = ((CafeRoom) AbstractDungeon.getCurrRoom().event).getCurrentInhabitants();
-        //TODO: Exclude seen pages if there are enough unseen and mark as seen
+        List<String> seenPages = getSeenPages();
         WeightedList<AbstractPage> pages = new WeightedList<>();
+        List<AbstractPage> filteredPages = new ArrayList<>();
 
         for(AbstractPage p : allPages) {
             if(p.canSpawn()) {
-                pages.add(p, p.preferredSpawn(inhabitants) ? 6 : 1);
+                if(!seenPages.contains(p.id)) {
+                    pages.add(p, p.preferredSpawn(inhabitants) ? 30 : 5); // Might want to add some dynamic logic in case there are a lot of pages
+                } else {
+                    filteredPages.add(p);
+                }
             }
+        }
+        if(pages.size() < NUM_PAGES) {
+            pages.addAll(filteredPages, 1); // Reduced so that the last few pages have a higher chance to be seen
         }
 
         for (int i = 0; i < NUM_PAGES; i++) {
-            selectedPages.add(pages.getRandom(AbstractDungeon.miscRng, true));
+            AbstractPage p = pages.getRandom(AbstractDungeon.miscRng, true);
+            selectedPages.add(p);
+            if(!filteredPages.contains(p)) {
+                addSeenPage(p.id);
+            }
+        }
+
+        //TODO: Create cutscene and add to it
+    }
+
+    public static List<String> getSeenPages() {
+        List<String> seenPages = new ArrayList<>();
+        if (Anniv7Mod.modConfig != null) {
+            seenPages = Arrays.asList(Anniv7Mod.modConfig.getString(PAGE_CONFIG_KEY).split(","));
+        }
+        return seenPages;
+    }
+
+    public static boolean hasSeenPage(String pageId) {
+        return Anniv7Mod.modConfig != null &&
+                Anniv7Mod.modConfig.getString(PAGE_CONFIG_KEY).contains(pageId+",");
+    }
+
+    public static void addSeenPage(String pageId) {
+        if (Anniv7Mod.modConfig != null) {
+            String pageList = Anniv7Mod.modConfig.getString(PAGE_CONFIG_KEY);
+            Anniv7Mod.modConfig.setString(PAGE_CONFIG_KEY, pageList + pageId + ",");
+            try {
+                Anniv7Mod.modConfig.save();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
