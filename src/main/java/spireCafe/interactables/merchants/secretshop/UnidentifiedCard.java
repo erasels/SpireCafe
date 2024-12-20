f376f9d56aa4e715d6b6cdad3bdfa3e2bf28790d
Anthony Mercado <mercado.anthony.e@gmail.com>
2024-12-20 11:27:29 -0500

    Unidentified Card cleanup

--- src/main/java/spireCafe/interactables/merchants/secretshop/UnidentifiedCard.java
+++ src/main/java/spireCafe/interactables/merchants/secretshop/UnidentifiedCard.java
@@ -3,6 +3,7 @@ package spireCafe.interactables.merchants.secretshop;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.EnumSet;
+import basemod.AutoAdd;
 
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.red.IronWave;
