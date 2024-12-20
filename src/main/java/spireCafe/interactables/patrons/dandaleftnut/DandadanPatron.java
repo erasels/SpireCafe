1a4cac4432f01ddade5fe31347aa9c2eaff112d6
Keurodz <77088132+Keurodz@users.noreply.github.com>
2024-12-19 22:20:35 -0600

    Dandadanleftnut folder creation

---
+++ src/main/java/spireCafe/interactables/patrons/dandaleftnut/DandadanPatron.java
@@ -0,0 +1,21 @@
+package spireCafe.interactables.patrons.dandaleftnut;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import spireCafe.abstracts.AbstractPatron;
+
+public class DandadanPatron extends AbstractPatron {
+
+    public DandadanPatron(float animationX, float animationY, float hb_w, float hb_h) {
+        super(animationX, animationY, hb_w, hb_h);
+    }
+
+    @Override
+    public void onInteract() {
+
+    }
+
+    @Override
+    public void renderCutscenePortrait(SpriteBatch sb) {
+
+    }
+}
