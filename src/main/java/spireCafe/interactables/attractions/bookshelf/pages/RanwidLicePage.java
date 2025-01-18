863d33282af853bc3c5e964b88a8e647390105e6
Enbeon <85409980+ActuallyEnbeon@users.noreply.github.com>
2025-01-18 15:02:37 +0000

    Make Ranwid's Lice page more likely to appear if Red Louse is a patron

--- src/main/java/spireCafe/interactables/attractions/bookshelf/pages/RanwidLicePage.java
+++ src/main/java/spireCafe/interactables/attractions/bookshelf/pages/RanwidLicePage.java
@@ -1,6 +1,10 @@
 package spireCafe.interactables.attractions.bookshelf.pages;
 
 import spireCafe.Anniv7Mod;
+import spireCafe.abstracts.AbstractCafeInteractable;
+import spireCafe.interactables.patrons.redlouse.RedLousePatron;
+
+import java.util.List;
 
 public class RanwidLicePage extends AbstractPage {
     private static final String ID = Anniv7Mod.makeID(RanwidLicePage.class.getSimpleName());
@@ -21,5 +25,10 @@ public class RanwidLicePage extends AbstractPage {
         textMaker.append(uiStrings.TEXT[1]); // New lines
         textMaker.append(uiStrings.TEXT[2]); // Entry
         return textMaker.toString();
+    }
+
+    @Override
+    public boolean preferredSpawn(List<AbstractCafeInteractable> currentInhabitants) {
+        return currentInhabitants.stream().anyMatch(p -> p instanceof RedLousePatron);
     }
 }
