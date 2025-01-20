d7471943e95d72cd2c7583fb889dcac2643e1578
iryan0702 <iryan0702@gmail.com>
2025-01-19 18:24:53 -0800

    Transient Patron: Add chillin scene to cutscene (very chill)

--- src/main/java/spireCafe/interactables/patrons/thetransient/TheTransientCutscene.java
+++ src/main/java/spireCafe/interactables/patrons/thetransient/TheTransientCutscene.java
@@ -4,14 +4,17 @@ import basemod.ReflectionHacks;
 import basemod.cardmods.InnateMod;
 import basemod.helpers.CardModifierManager;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.evacipated.cardcrawl.mod.stslib.fields.cards.AbstractCard.AutoplayField;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.PowerTip;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 import com.megacrit.cardcrawl.relics.BurningBlood;
+import com.megacrit.cardcrawl.vfx.AbstractGameEffect;
 import com.megacrit.cardcrawl.vfx.cardManip.PurgeCardEffect;
 import spireCafe.Anniv7Mod;
 import spireCafe.abstracts.AbstractCutscene;
@@ -20,6 +23,9 @@ import spireCafe.cardmods.AutoplayMod;
 import spireCafe.cardmods.TransientMod;
 import spireCafe.util.cutsceneStrings.CutsceneStrings;
 import spireCafe.util.cutsceneStrings.LocalizedCutsceneStrings;
+import spireCafe.vfx.SimpleTextureLerp;
+
+import java.util.ArrayList;
 
 import static spireCafe.Anniv7Mod.makeID;
 
@@ -51,6 +57,9 @@ public class TheTransientCutscene extends AbstractCutscene {
                 this.dialog.addDialogOption(OPTIONS[1]).setOptionResult((i)-> goToDialogue(7));
                 break;
             case 3:
+                for(SimpleTextureLerp e: textureLerps){
+                    e.earlyEnd();
+                }
                 nextDialogue();
 
                 this.dialog.addDialogOption(OPTIONS[5]).setOptionResult((i)->{
@@ -65,7 +74,10 @@ public class TheTransientCutscene extends AbstractCutscene {
                     AbstractDungeon.gridSelectScreen.open(group, 1, OPTIONS[4], false);
 
                     character.alreadyPerformedTransaction = true;
-                    if(fullyFaded){
+                    if(chillTime < 5){
+                        goToDialogue(8);
+                        character.blockingDialogueIndex = 2;
+                    }else if(fullyFaded){
                         goToDialogue(6);
                         character.blockingDialogueIndex = 1;
                     }else{
@@ -77,6 +89,7 @@ public class TheTransientCutscene extends AbstractCutscene {
             case 5:
             case 6:
             case 7:
+            case 8:
                 ((TheTransientPatron)character).postCutsceneFade();
                 endCutscene();
                 break;
@@ -88,20 +101,68 @@ public class TheTransientCutscene extends AbstractCutscene {
     }
 
     float chillTime = 0;
+    int chillInd = -1;
+    float speedMult = 1; //sequence speeds up over time by speedMult, lerp effects speeds up by half as much (lots of things on screen over time)
     boolean fullyFaded = false;
-    public static final float CHILL_FADE_TIME = 30;
+    public static final float CHILL_FADE_TIME = 60;
+    ArrayList<SimpleTextureLerp> textureLerps = new ArrayList<>();
     //grid selection for applying transient mod
     public void update() {
         super.update();
+
+        //chillmode
         if(dialogueIndex == 3){
-            chillTime += Gdx.graphics.getDeltaTime();
+            chillTime += Gdx.graphics.getDeltaTime() * speedMult;
+
+            if(chillTime % CHILL_FADE_TIME <= 5){
+                chillInd = -1;
+            }else if(chillInd < 0 && chillTime % CHILL_FADE_TIME > 8){ //right to left transient
+                chillInd = 0;
+                SimpleTextureLerp e = new SimpleTextureLerp(character.cutscenePortrait.getTexture(), Settings.WIDTH*1.8f, Settings.HEIGHT*0.2f, 0, 1.6f, 0.8f, Settings.WIDTH*0.2f, Settings.HEIGHT*0.2f, 0, 1.8f, 0, 20f/((speedMult+1f)/2));
+                AbstractDungeon.effectsQueue.add(e);
+                textureLerps.add(e);
+            } else if (chillInd < 1 && chillTime % CHILL_FADE_TIME > 18) { //spinny transient
+                chillInd = 1;
+                SimpleTextureLerp e = new SimpleTextureLerp(character.cutscenePortrait.getTexture(), Settings.WIDTH/2f, Settings.HEIGHT/2f, 0, 0, 1, Settings.WIDTH/2f, Settings.HEIGHT/2f, 480, 1.5f, 0, 25f/((speedMult+1f)/2));
+                AbstractDungeon.effectsQueue.add(e);
+                textureLerps.add(e);
+            } else if (chillInd < 2 && chillTime % CHILL_FADE_TIME > 30) { //left to right player
+                chillInd = 2;
+                SimpleTextureLerp e = new SimpleTextureLerp(AbstractDungeon.player.shoulder2Img, 0, Settings.HEIGHT/2f, 0, 1, 1, Settings.WIDTH, Settings.HEIGHT/2f, 0, 1, 0, 20f/((speedMult+1f)/2));
+                AbstractDungeon.topLevelEffectsQueue.add(e);
+                textureLerps.add(e);
+            } else if (chillInd < 3 && chillTime % CHILL_FADE_TIME > 36) { //drop down transient
+                chillInd = 3;
+                SimpleTextureLerp e = new SimpleTextureLerp(character.cutscenePortrait.getTexture(), Settings.WIDTH*0.75f, Settings.HEIGHT*1.8f, 0, 1.2f, 0.8f, Settings.WIDTH*0.75f, Settings.HEIGHT*0.2f, 0, 1.2f, 0, 25f/((speedMult+1f)/2));
+                AbstractDungeon.topLevelEffectsQueue.add(e);
+                textureLerps.add(e);
+            } else if (chillInd < 4 && chillTime % CHILL_FADE_TIME > 42) { //spinning player
+                chillInd = 4;
+                SimpleTextureLerp e = new SimpleTextureLerp(AbstractDungeon.player.shoulder2Img, 0, Settings.HEIGHT/2f, 0, 2, 1, Settings.WIDTH/2f, Settings.HEIGHT/2f, -800, 0, 0, 30f/((speedMult+1f)/2));
+                AbstractDungeon.topLevelEffectsQueue.add(e);
+                textureLerps.add(e);
+            } else if (chillInd < 5 && chillTime % CHILL_FADE_TIME > 50) { //both slide from sides of screen
+                chillInd = 5;
+                speedMult *= 1.2f; //oh my god is it getting faster
+                SimpleTextureLerp e = new SimpleTextureLerp(character.cutscenePortrait.getTexture(), Settings.WIDTH*-0.5f, Settings.HEIGHT/2f, 0, 1, 1, Settings.WIDTH, Settings.HEIGHT/2f, 0, 0, 0, 20f/((speedMult+1f)/2));
+                AbstractDungeon.topLevelEffectsQueue.add(e);
+                textureLerps.add(e);
+                e = new SimpleTextureLerp(AbstractDungeon.player.shoulder2Img, Settings.WIDTH*1.5f, Settings.HEIGHT/2f, 0, 1, 1, 0, Settings.HEIGHT/2f, 0, 0, 0, 20f/((speedMult+1f)/2));
+                AbstractDungeon.topLevelEffectsQueue.add(e);
+                textureLerps.add(e);
+            }
+
+            //fade cutscene transient over time of chillin
             if(character instanceof TheTransientPatron){
                 ((TheTransientPatron) character).cutsceneAlpha = Math.max(0, (CHILL_FADE_TIME-chillTime)/CHILL_FADE_TIME);
             }
+            //mark the point which the player has fully chilled for transient to fade
             if(chillTime > CHILL_FADE_TIME){
                 fullyFaded = true;
             }
         }
+
+        //simple grid select logic
         if (!AbstractDungeon.gridSelectScreen.selectedCards.isEmpty()) {
             for (int i = 0; i < AbstractDungeon.gridSelectScreen.selectedCards.size(); i++) {
                 AbstractCard c = AbstractDungeon.gridSelectScreen.selectedCards.get(i);
@@ -110,5 +171,10 @@ public class TheTransientCutscene extends AbstractCutscene {
             AbstractDungeon.gridSelectScreen.selectedCards.clear();
             backToCutscene();
         }
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        super.render(sb);
     }
 }
