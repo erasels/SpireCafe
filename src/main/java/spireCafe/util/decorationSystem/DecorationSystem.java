0116f2290f594727fdf89a187fa434de5be74144
Kiooeht <kiooeht@gmail.com>
2025-01-22 19:34:54 -0800

    Make sign render after other wall decorations

--- src/main/java/spireCafe/util/decorationSystem/DecorationSystem.java
+++ src/main/java/spireCafe/util/decorationSystem/DecorationSystem.java
@@ -35,8 +35,10 @@ public class DecorationSystem {
     private void spawnDecorations() {
         Collections.shuffle(allDecorations, new java.util.Random(rng.randomLong()));
 
+        // Add sign first so other decorations try to avoid overlapping it
+        BarSignDecoration sign = new BarSignDecoration();
+        decorations.add(sign);
         initWindows();
-        decorations.add(new BarSignDecoration());
         boolean hasLarge = false;
         int decorationAmt = NUM_DECOS;
         for (int i = 0; i < decorationAmt; i++) {
@@ -49,6 +51,9 @@ public class DecorationSystem {
                 decorationAmt++;
             }
         }
+        // Move sign to end of list so it renders on top of other wall decorations
+        decorations.remove(sign);
+        decorations.add(sign);
     }
 
     private void initDeco(Decoration deco) {
