533619d8b7f1451aa681b037bbcb151f8ea213a4
Indi <indiesneault@gmail.com>
2025-01-27 21:18:46 -0800

    use cardQueueAtion & make awakened relic text green

--- src/main/java/spireCafe/interactables/patrons/dandadan/GhostModifier.java
+++ src/main/java/spireCafe/interactables/patrons/dandadan/GhostModifier.java
@@ -7,13 +7,12 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.GameActionManager;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.CardQueueItem;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.UIStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
@@ -92,24 +91,7 @@ public class GhostModifier extends AbstractCardModifier {
     @Override
     public void onUse(AbstractCard card, AbstractCreature target, UseCardAction action) {
         if (playTwice && !card.purgeOnUse) {
-            AbstractMonster m = null;
-            if (action.target != null) {
-                m = (AbstractMonster) action.target;
-            }
-
-            AbstractCard tmp = card.makeSameInstanceOf();
-            AbstractDungeon.player.limbo.addToBottom(tmp);
-            tmp.current_x = card.current_x;
-            tmp.current_y = card.current_y;
-            tmp.target_x = Settings.WIDTH / 2.0F - 300.0F * Settings.scale;
-            tmp.target_y = Settings.HEIGHT / 2.0F;
-            if (m != null) {
-                tmp.calculateCardDamage(m);
-            }
-
-            tmp.purgeOnUse = true;
-            AbstractDungeon.actionManager.addCardQueueItem(new CardQueueItem(tmp, m, card.energyOnUse, true, true),
-                    true);
+            GameActionManager.queueExtraCard(card.makeStatEquivalentCopy(), (AbstractMonster) target);
         }
     }
 
