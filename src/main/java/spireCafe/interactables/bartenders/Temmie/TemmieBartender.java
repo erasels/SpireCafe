6268fbe4b81f98e4680aeb2f7be921399c1c2f0f
UltimateEnemyArmoredTemmie <nekoconeko1050@gmail.com>
2025-02-05 03:12:18 -0800

    Added Cafe-Bartender Temmie. Messy code, still working on audio when interacting with Temmie.

--- src/main/java/spireCafe/interactables/bartenders/Temmie/TemmieBartender.java
+++ src/main/java/spireCafe/interactables/bartenders/Temmie/TemmieBartender.java
@@ -1,61 +1,28 @@
 package spireCafe.interactables.bartenders.Temmie;
 
-import basemod.helpers.CardModifierManager;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CharacterStrings;
-import com.megacrit.cardcrawl.vfx.UpgradeShineEffect;
-import org.apache.commons.lang3.math.NumberUtils;
 import spireCafe.Anniv7Mod;
 import spireCafe.abstracts.AbstractBartender;
 import spireCafe.abstracts.BartenderCutscene;
-import spireCafe.cardmods.MoreBlockMod;
 import spireCafe.util.TexLoader;
 import spireCafe.util.Wiz;
 import spireCafe.util.cutsceneStrings.CutsceneStrings;
 import spireCafe.util.cutsceneStrings.LocalizedCutsceneStrings;
 
 import java.util.List;
-import java.util.Random;
 
 public class TemmieBartender extends AbstractBartender {
     public static final String ID = TemmieBartender.class.getSimpleName();
     private static final CharacterStrings characterStrings = CardCrawlGame.languagePack.getCharacterString(Anniv7Mod.makeID(ID));
     private static final CutsceneStrings cutsceneStrings = LocalizedCutsceneStrings.getCutsceneStrings(Anniv7Mod.makeID(ID + "Cutscene"));
 
-    private static final int FLAKE_COST = 10;
-    private static final int FLAKE_PRICE_MULTIPLIER = 2;
-
-    private int flakePrice = FLAKE_COST;
-
-    private static final String[] activationPhrases = {
-            "hoi hoi!! welcome to... temmie bar!!!",
-            "yay!!! a custOmer!!! temmie sooo happy!!!",
-            "u came to drink??? temmie came to... uh... sell drinks!!!",
-            "oh wowie!!! another hooman/doge/monster/??? in temmie bar!!!",
-            "hoi!!! wanna try da bestest drinks in... uh... ever?!?!",
-            "u look like u need... refreshment!!! temmie has da goods!!!",
-            "wowwowwow!!! temmie never see customer so cool!!! wanna buy stuff???",
-            "drink up!!! wait, u need to buy first... then drink up!!!",
-            "heh heh... temmie gots exclusive temmie product... wanna see???",
-            "u thirsty?? u hungry??? u just wanna give temmie munnie??? all acceptable!!!"
-    };
-
-    private static final String[] flakesPhrases = {
-            "hoi!!! temmie flakes!!! taste like... uh... food!!!",
-            "wanna eat??? wanna spend??? temmie flakes solve both!!!",
-            "great nutrishun!! heal 1 hp!!! only cost... uh... what was price again???",
-            "limited edition!!! (but unlimited stock!)",
-            "buy now!!! next one even more exclusive!!!",
-            "each temmie flake better than last!!! science fact!!!",
-            "mmmm… temmie flakes… cronchy, fluffy, and... probably edible!!!",
-            "wow!! u already bought one?? wanna spend even more munnie?!?!",
-            "heal 1 hp!!! price go up each time!!! perfect investment!!!",
-            "buy now or regret later... or buy later... then regret more!!!"
-    };
+    private static final int FLAKE_COST = 1; // Starting cost for a Temmie Flake
+    private static final int FLAKE_PRICE_MULTIPLIER = 1; // No price increase for repeat purchases
+    private int flakePrice = FLAKE_COST; // Current price of Temmie Flake
 
     public TemmieBartender(float animationX, float animationY) {
         super(animationX, animationY, 180.0f, 250.0f);
@@ -67,10 +34,40 @@ public class TemmieBartender extends AbstractBartender {
 
     @Override
     public void onInteract() {
-        Random rand = new Random();
-        int phraseIndex = rand.nextInt(activationPhrases.length);
-        CardCrawlGame.sound.play("/anniv7Resources/audio/Temmie-Voice.mp3");
+        // Debug logging
+        System.out.println("Interacting with Temmie Bartender");
+
+        // Play Temmie voice when bartender is clicked
+        CardCrawlGame.sound.play(Anniv7Mod.makeID("audio/Temmie-Voice2.ogg"));
+
+        // Show the bartender cutscene
         AbstractDungeon.topLevelEffectsQueue.add(new BartenderCutscene(this, cutsceneStrings));
+    }
+
+    @Override
+    public String getNoThanksDescription() {
+        // Play sound when declining the offer
+        CardCrawlGame.sound.play(Anniv7Mod.makeID("audio/Temmie-Voice2.ogg"));
+        return cutsceneStrings.OPTIONS[0];
+    }
+
+    @Override
+    public void applyHealAction() {
+        // Play sound when the player buys Temmie Flakes
+        CardCrawlGame.sound.play(Anniv7Mod.makeID("audio/Temmie-Voice2.ogg"));
+
+        // Apply healing and gold deduction
+        Wiz.p().heal(getHealAmount());
+        Wiz.p().loseGold(flakePrice);
+    }
+
+    @Override
+    public void applySecondOptionAction() {
+        // Play sound when selecting a secondary option (if applicable)
+        CardCrawlGame.sound.play(Anniv7Mod.makeID("audio/Temmie-Voice2.ogg"));
+
+        // Important to reset this action flag
+        inSecondAction = false;
     }
 
     @Override
@@ -85,26 +82,11 @@ public class TemmieBartender extends AbstractBartender {
 
     @Override
     public int getHealAmount() {
-        return (Wiz.p().currentHealth < Wiz.p().maxHealth) ? 1 : 0;
+        return (Wiz.p().currentHealth < Wiz.p().maxHealth) ? 10 : 0;
     }
 
     @Override
-    public void applyHealAction() {
-        CardCrawlGame.sound.play("SLEEP_1-2");
-        Wiz.p().heal(getHealAmount());
-        Wiz.p().loseGold(flakePrice);
-        flakePrice *= FLAKE_PRICE_MULTIPLIER;
-        inHealAction = false;
-    }
-
-    @Override
-    public void doForSelectedCardsFromSecondAction(List<AbstractCard> selected) {
-    }
-
-    @Override
-    public String getNoThanksDescription() {
-        return cutsceneStrings.OPTIONS[0];
-    }
+    public void doForSelectedCardsFromSecondAction(List<AbstractCard> selected) {}
 
     @Override
     public String getLabelText() {
