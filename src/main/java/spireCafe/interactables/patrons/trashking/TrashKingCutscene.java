e54e1d6c8f793fa223e57777693ce87770aefcaa
Cany0udance <blakkrum@aol.com>
2025-02-11 08:18:20 -0500

    removed problematic relics

--- src/main/java/spireCafe/interactables/patrons/trashking/TrashKingCutscene.java
+++ src/main/java/spireCafe/interactables/patrons/trashking/TrashKingCutscene.java
@@ -4,7 +4,6 @@ import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.RelicLibrary;
-import com.megacrit.cardcrawl.potions.PotionSlot;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 import spireCafe.abstracts.AbstractCutscene;
 import spireCafe.abstracts.AbstractNPC;
@@ -28,7 +27,7 @@ public class TrashKingCutscene extends AbstractCutscene {
             PetRock.ID, PaperNail.ID, SuperheroComic.ID, RingOfTheNoodle.ID,
             Supplements.ID, HospitalBill.ID, WellDoneSteak.ID, LonelyAnt.ID, NapiersBones.ID,
             LostPenny.ID, Popcorn.ID, InsuranceCard.ID, TyrtleShell.ID, RayOfMoonlight.ID,
-            StylePoints.ID, Loan.ID, PaintBucket.ID, ElyphantToothpaste.ID, HeirloomFork.ID,
+            StylePoints.ID, PaintBucket.ID, HeirloomFork.ID,
             FistOfThePugilist.ID, RedButton.ID, Neurotoxin.ID
     };
 
@@ -103,27 +102,13 @@ public class TrashKingCutscene extends AbstractCutscene {
         AbstractDungeon.player.loseGold(cost);
         character.alreadyPerformedTransaction = true;
 
-        ArrayList<AbstractRelic> givenRelics = new ArrayList<>();
-
         ArrayList<String> availableRelics = new ArrayList<>(Arrays.asList(TRASH_KING_RELICS));
-        availableRelics.removeIf(relicId ->
-                AbstractDungeon.player.hasRelic(relicId) ||
-                        (relicId.equals(ElyphantToothpaste.ID) && !hasAnyPotions())
-        );
+
         Collections.shuffle(availableRelics, new Random(AbstractDungeon.miscRng.randomLong()));
 
         for (int i = 0; i < relicCount && i < availableRelics.size(); i++) {
             AbstractRelic relic = RelicLibrary.getRelic(availableRelics.get(i)).makeCopy();
             AbstractDungeon.getCurrRoom().spawnRelicAndObtain(Settings.WIDTH / 2, Settings.HEIGHT / 2, relic);
-            givenRelics.add(relic);
-        }
-
-        if (relicCount == 2) {
-            for (AbstractRelic relic : givenRelics) {
-                if (relic.relicId.equals(ElyphantToothpaste.ID) || relic.relicId.equals(Loan.ID)) {
-                    relic.onEquip(); // onEquip wasn't working in the two-relic purchase scenario, for some reason. Have to call it manually
-                }
-            }
         }
 
         if (relicCount == 1) {
@@ -133,11 +118,6 @@ public class TrashKingCutscene extends AbstractCutscene {
             goToDialogue(7);
             this.dialogueIndex = 8;
         }
-    }
-
-    private boolean hasAnyPotions() {
-        return AbstractDungeon.player.potions.stream()
-                .anyMatch(potion -> !(potion instanceof PotionSlot));
     }
 
     @Override
